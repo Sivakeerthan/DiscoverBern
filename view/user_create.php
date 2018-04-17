fbbc0ceba558fd81df2db33eabc026552ca7b6b0
Sivakeerthan <keerthan_1@hotmail.com>
2018-04-17 13:25:58 +0200

    Registration Complete

--- view/user_create.php
+++ view/user_create.php
@@ -1,15 +1,9 @@
 <form class="form-horizontal" action="/user/doCreate" method="post">
 	<div class="component" data-html="true">
 		<div class="form-group">
-		  <label class="col-md-2 control-label" for="firstName">Vorname</label>
+		  <label class="col-md-2 control-label" for="username">Benutzername</label>
 		  <div class="col-md-4">
-		  	<input id="firstName" name="firstName" type="text" placeholder="Vorname" class="form-control input-md">
-		  </div>
-		</div>
-		<div class="form-group">
-		  <label class="col-md-2 control-label" for="lastName">Nachname</label>
-		  <div class="col-md-4">
-		  	<input id="lastName" name="lastName" type="text" placeholder="Nachname" class="form-control input-md">
+		  	<input id="usernamee" name="username" type="text" placeholder="Benutzername" class="form-control input-md">
 		  </div>
 		</div>
 		<div class="form-group">
@@ -19,9 +13,14 @@
 		  </div>
 		</div>
 		<div class="form-group">
-		  <label class="col-md-2 control-label" for="password">Passwort</label>
+		  <label class="col-md-2 control-label" for="password1">Passwort</label>
 		  <div class="col-md-4">
-		  	<input id="password" name="password" type="password" placeholder="Passwort" class="form-control input-md">
+		  	<input id="password1" name="password1" type="password" placeholder="Passwort" class="form-control input-md">
+		  </div>
+		</div><div class="form-group">
+		  <label class="col-md-2 control-label" for="password2">Passwort bestätigen</label>
+		  <div class="col-md-4">
+		  	<input id="password2" name="password2" type="password" placeholder="Passwort" class="form-control input-md">
 		  </div>
 		</div>
 		<div class="form-group">
@@ -30,5 +29,8 @@
 		    <input id="send" name="send" type="submit" class="btn btn-primary">
 		  </div>
 		</div>
+        <div class="form-group">
+            <label class="col-md-2 control-label" name="meldung" id="meldung"><?php $uc = new UserController(); echo $uc->meldung; ?></label>
+        </div>
 	</div>
 </form>
