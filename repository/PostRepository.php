67195dd1393b4c37c8f12f11a2ab0d6b461a1422
MischuNR <brohrm@bbc-be.local>
2018-04-18 16:15:13 +0200

    Ort.csv für PLZ und Ort eingefügt, Post Repository verändert

--- repository/PostRepository.php
+++ repository/PostRepository.php
@@ -4,7 +4,10 @@ class PostRepository extends Repository{
 
     protected $tableName='post';
 
-    
+    public function toArray($category)
+    {
+      $query = "SELECT imgurl FROM post WHERE password = sha1(?)";
+    }
 
 }
 
