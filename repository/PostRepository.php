4165733808a7023187dffb8b1915061e997f4e1e
MischuNR <brohrm@bbc-be.local>
2018-04-19 08:47:47 +0200

    PostRepository.php

--- repository/PostRepository.php
+++ repository/PostRepository.php
@@ -4,9 +4,26 @@ class PostRepository extends Repository{
 
     protected $tableName='post';
 
-    public function toArray($category)
+    public function readByCategory($category)
     {
-      $query = "SELECT imgurl FROM post WHERE password = sha1(?)";
+      $query = "SELECT imgurl FROM post WHERE category = ?";
+      $statement = ConnectionHandler::getConnection()->prepare($query);
+      $statement->bind_param('s', $category);
+      $statement->execute();
+
+      $result = $statement->get_result();
+      if (!$result) {
+          throw new Exception($statement->error);
+      }
+
+      // Datensätze aus dem Resultat holen und in das Array $rows speichern
+      $rows = array();
+      while ($row = $result->fetch_object()) {
+          $rows[] = $row;
+      }
+
+      return $rows;
+
     }
 
 }
